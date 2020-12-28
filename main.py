62a5213bd3a587d26996e0baf2bc8696e840fc39
TheSkripton <71188627+TheSkripton@users.noreply.github.com>
2020-12-28 21:30:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ AI.setup()
 
 while 1==1:
     time.sleep(1)
-    newmessage=pickle.load(open("Ai\data.txt","rb"))
+    newmessage=pickle.load(open("data.txt","rb"))
     messagetobesent=AI.executeall(newmessage[0])
     if(newmessage[1]=='u' and messagetobesent!="no"):
         Sendmesage.sendmessage(messagetobesent)
         newmessage=[newmessage[0],'r']
-        pickle.dump(newmessage,open("Ai\data.txt","wb"))
+        pickle.dump(newmessage,open("data.txt","wb"))
         print(newmessage)
         print(messagetobesent)
